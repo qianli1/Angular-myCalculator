da523c38314995eb7a6ae637caa986bfc0e2ff23
qianli1 <liqian689@hotmail.com>
2022-03-04 14:03:10 -0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myCalculator';
+  pageTitle = "Basic Calculator";
+  displayValue: string = '';
+  num0 = "0";
+  num1 = "1";
+  num2 = "2";
+  num3 = "3";
+  num4 = "4";
+  num5 = "5";
+  num6 = "6";
+  num7 = "7";
+  num8 = "8";
+  num9 = "9";
+  signPlus = "+";
+  signMinus = "-";
+  signMultiply = "x";
+  signClear = "C";
+  signEqual = "=";
+  signDivide = "&#247;";
+
+
+  calculate(c: string) {
+    if (c === "x") {
+      this.displayValue += '*';
+    } else if (c === "&#247;") {
+      this.displayValue += '/';
+    } else if (c === "C") {
+      this.displayValue = '';
+    } else if (c === "=") {
+      this.displayValue = eval(this.displayValue);
+    } else
+    this.displayValue += c;
+  }
 }
